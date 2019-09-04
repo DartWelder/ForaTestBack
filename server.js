f150ea5e60e8015dbcdf1e5e15bc5a73bfeb68e9
DartWelder <alexey.evenkov@gmail.com>
2019-09-04 19:05:29 +0300

    enpoints

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-import express from 'express';
-import http from 'http';
-import socket from 'socket.io';
+import express from 'express'
+import http from 'http'
+import socket from 'socket.io'
+import cors from 'cors'
+import uuid from 'uuid'
 
 const app = express();
 const server = http.createServer(app);
@@ -10,6 +12,17 @@ server.listen(3001);
 
 let rooms = {};
 
+app.use(cors())
+
+app.get('/getrooms', (req, res) => {
+    res.send(getRoomKeys());
+})
+
+app.get('/createroom', (req, res) => {
+    rooms[uuid()] = [];
+    res.send(getRoomKeys());
+})
+
 io.sockets.on('connection', (socket) => {
     socket.on('addMessage', (data) => {
         if (roomExists(data.roomId)) {
@@ -18,14 +31,29 @@ io.sockets.on('connection', (socket) => {
             rooms[data.roomId] = [data];
         }
         io.sockets.emit('messageSent', rooms[data.roomId])
-    })
+    });
     socket.on('getInitialMessages', (id) => {
         let response;
         response = roomExists(id) ? rooms[id] : [];
         io.sockets.emit('initialMessagesProvided', response)
-    })
-
-    function roomExists (id) {
-        return rooms.hasOwnProperty(id);
-    }
+    });
+    socket.on('isWriting', (data) => {
+        console.log(`notification-${data.roomId}`)
+        io.sockets.emit(`notification-${data.roomId}`, {
+            type: 'isWriting',
+            user: data.user
+        })
+    });
 });
+
+let roomExists = (id) => {
+    return rooms.hasOwnProperty(id);
+}
+
+let getRoomKeys = () => {
+    return Object.entries(rooms)
+        .map(([key, value]) => {
+            console.log(key)
+            return key;
+        })
+}
